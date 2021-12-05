e4c3f6467dbd680d0289c7480e04eac231f576f6
Bryan A <bryanacostadelacruz@gmail.com>
2021-12-04 23:58:56 -0600

    comments + README

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Weather-Dashboard
+# Work Day Scheduler
+
+GitHub Link : https://github.com/TheBryan-Acosta/Weather-Dashboard
+GitPages Link : https://thebryan-acosta.github.io/Weather-Dashboard/
+
+## **Description**
+
+Challenge 06 This Weeks challenge was to create a Weather Dashboard
+
+### **What was developed**
+
+A mobile responsive weather app that displays the current weather, and its five day forecast from a given city.
+
+### **Development** -
+
+For the weather data, I used two server-side apis, one to get the longitude and latitude from a city,
+and another to use those values to get the weather information, using OpenWeatherMap Onecall & GeoLocation api.
+Mobile responsiveness and gid layout were thanks to bootstrap and media queries.
+Js libraries used were MomentJs for dates and times, and jquery.
+
+## **Examples**
+
+## **Installation**
+
+Developed with HTML, CSS, js through Visual Studio Code.
+
+## **Usage**
+
+Checkout the current weather and forecast!
+
+## **Credits**
+
+University of Texas Coding Bootcamp
+
+## **License**
+
+MIT License
+
+Copyright (c) [2021] [Bryan Acosta De La Cruz]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 var searchForm = document.getElementById("citySearch");
 
+//load local storage
 loadLocalStorage();
 function loadLocalStorage(){
+    //assign Localstorage arr to a variable
     var histNames = JSON.parse(localStorage.getItem('searchHistory'));
+
+    //check if histNames is a valid variable
     if(histNames){
+        //iterate from the last index to the first
         for(i=histNames.length - 1; i >= 0 ; i--){
+            //pass the city name at the specific index to button creation function
             addToHistory(histNames[i]);
         }
     }
@@ -12,12 +18,13 @@ function loadLocalStorage(){
 };
 
 //Current Weather Data Call
-function GetCurrentWeatherData(cityName,lat,lon){
+function GetCurrentWeatherData(lat,lon){
     var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=minutely,hourly,alerts&units=imperial&appid=5a204b8bb4ef44421eed155c5a0fefac';
 
     fetch(apiUrl).then(function(response) {
         if (response.ok){
             response.json().then(function(data) {
+                //pass weather data to display
                 displayCurrentWeatherData(data);
             });
         }
@@ -35,13 +42,16 @@ function GetLatLong(cityName){
     fetch(apiUrl).then(function(response) {
         if (response.ok){
             response.json().then(function(data) {
-
+                // checks if input was valid(data was aquired)
                 if(data.length != 0){
+                //set the current weather header to city name and current date
                 $("#curC").text(data[0].name + ' ' + moment().format('L'));
-                GetCurrentWeatherData(cityName, data[0].lat, data[0].lon);
+                // pass latitude and longitude to openWeather call
+                GetCurrentWeatherData(data[0].lat, data[0].lon);
+                // create a history button with city name
                 addToHistory(data[0].name);
                 }
-
+                // rejects input and alerts user of a invalid input
                 else{
                     alert("Enter a valid city name!");
                 }
@@ -54,7 +64,7 @@ function GetLatLong(cityName){
     });
 };
 
-//on submit
+//on submit form
 function formHandler(event) {
     event.preventDefault();
     var cityName = document.getElementById("cityname").value.trim();
@@ -62,7 +72,6 @@ function formHandler(event) {
     if(cityName){
         GetLatLong(cityName);
     }
-
     else{
         alert("Please enter a City name");
     }
@@ -74,25 +83,29 @@ searchForm.addEventListener("submit", formHandler);
 //fill current weather data
 function displayCurrentWeatherData(weatherData){
 
+    // delete any uv El on new form submit
     $(".uvExist").each(function(){
        $(this).remove();
    });
-
+   // set our list text to temp,windspeed, and humidity
     var listEl = document.querySelector('ul');
     $("#curT").text('Temperature: ' + weatherData.current.temp + ' F');
     $("#curW").text('Windspeed: ' + weatherData.current.wind_speed + ' MPH');
     $("#curH").text('Humidity: '+ weatherData.current.humidity +'%');
 
+    // create our image tag and append to our header
     var cityNameEl = document.querySelector('#curC');
     var Image = document.createElement('img');
     Image.setAttribute("src", 'https://openweathermap.org/img/wn/'+ weatherData.current.weather[0].icon + '.png');
     cityNameEl.appendChild(Image);
 
+    //Here we create our uv index list item
     var UVI = document.createElement("li");
     UVI.setAttribute("class", "uvExist");
     var UVI_COLORBOX = document.createElement("span");
         UVI_COLORBOX.textContent =  weatherData.current.uvi;
 
+        //here is where we set the RED and GREEN color values based on our uv index
         if(weatherData.current.uvi >= 5){
             var UVI_RED = 255;
             var UVI_GREEN = 255 - (((weatherData.current.uvi) - 5)*51);
@@ -103,26 +116,35 @@ function displayCurrentWeatherData(weatherData){
             var UVI_GREEN = 255;
         }
 
+        //set css rgb(r,g,b) for the span background color
         UVI_COLORBOX.style.cssText = 'background-color: rgb('+ UVI_RED +', '+UVI_GREEN+', 51)';
         
 
+        //append span to Uv list item
         UVI.textContent = "UV Index: ";
         UVI.appendChild(UVI_COLORBOX);
 
+        //append uv list item to our list
         listEl.appendChild(UVI);
     
     displayFiveDayWeather(weatherData.daily)
 }
 
 
-
+// here we create our five day forcast boxes
 function displayFiveDayWeather(WD){
     $('#showForecast').text('Five day forecast');
+    //remove any past boxes
    $(".forebox").each(function(){
        $(this).remove();
    });
 
     var fivecontainer = document.getElementById("fivedaycontainer");
+
+    // create our tag, set its respective text from our weather data on its index
+    // since index 0 is our current day, start at the next index.
+    // append the tags to our div, with proper classes for bootstrap
+    // again for the uv index color is on a scale from 1-10 (red is 10)
     for(i=1; i < 6;i++){
         var forecast_container = document.createElement("div");
         var Temp = document.createElement("p");
@@ -177,7 +199,9 @@ function displayFiveDayWeather(WD){
     
 }
 
+// here I try to make the non repeating history buttons, to keep clutter off the page
 function addToHistory(cityName){
+    //create our button element
     var new_search =  document.createElement('button');
     var history_container = document.getElementById('prevSearch')
     new_search.className = "btn pastbtn col-12";
@@ -185,25 +209,30 @@ function addToHistory(cityName){
 
      var allPrev = history_container.querySelectorAll(".pastbtn");
    
+    //if there are buttons already
     if(allPrev[0]){
+        //dont do anything if the previous search is the same (we dont want stacking history of the same city)
         if(allPrev[0].textContent == new_search.textContent){
         }
 
         else {
+            //remove any city thats the same before we add it to our history
             $( '.pastbtn:contains(' + new_search.textContent + ')' ).remove();
             history_container.insertBefore(new_search, history_container.firstChild);
+            //Now we update our local storage
             StoreToLocal();
         }
      }
-
+     // if there are no history buttons
     else{
         history_container.insertBefore(new_search, history_container.firstChild);
         StoreToLocal();
     }
-
+    //clear our form
     $('#cityname').val('');
 };
 
+//create a variable to get all our city names and push it to our local storage in an array;
 function StoreToLocal(){
     var searchHistory = [];
     $(".pastbtn").each(function(){
@@ -213,6 +242,7 @@ function StoreToLocal(){
     localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
 }
 
+//on history button click, send to display our data
 $( "#prevSearch" ).on( "click", ".pastbtn", function() {
   GetLatLong($( this ).text());
 });
